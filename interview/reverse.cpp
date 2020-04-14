4a43ffbad9357a032649d4459b1c2684e9faac74
Maple24 <ylvis1024@gmail.com>
2020-04-14 15:11:00 +0800

    add print message in reverse.go

---
+++ interview/reverse.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+#include<assert.h>
+using namespace std;
+
+void reverse(int* number) {
+		assert(number) return;
+
+		char* number_str = new char[1024];
+		itoa(number, number_str, 10);
+		int start = 0;
+		int len = strlen(number_str);
+
+		if (number < 0) {
+				start++;
+				len++;
+		}
+
+		for (int i = start; i < len/2; i++) {
+				char temp = number_str[i];
+				number_str[i] = number_str[len - i - 1];
+				number[len - i - 1] = temp;
+		}
+
+		*number = atoi(number_str);
+		cout << *number << endl;
+
+}
