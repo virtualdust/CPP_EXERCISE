af05f768cd8afa3194efdda3b476400cf505db96
Maple24 <ylvis1024@gmail.com>
2020-04-15 22:48:11 +0800

    Format reverse.cpp

--- interview/reverse.cpp
+++ interview/reverse.cpp
@@ -6,29 +6,29 @@
 using namespace std;
 
 void reverse(int& number) {
-		char* number_str = new char[1024];
-		sprintf(number_str,"%d", number);
-		int start = 0;
-		int len = strlen(number_str);
+	char* number_str = new char[1024];
+	sprintf(number_str,"%d", number);
+	int start = 0;
+	int len = strlen(number_str);
 
-		if (number < 0) {
-				start++;
-				len++;
-		}
+	if (number < 0) {
+			start++;
+			len++;
+	}
 
-		for (int i = start; i < len/2; i++) {
-				char temp = number_str[i];
-				number_str[i] = number_str[len - i - 1];
-				number_str[len - i - 1] = temp;
-		}
+	for (int i = start; i < len/2; i++) {
+			char temp = number_str[i];
+			number_str[i] = number_str[len - i - 1];
+			number_str[len - i - 1] = temp;
+	}
 
-		number = atoi(number_str);
+	number = atoi(number_str);
 
 }
 
 int main(){
-		int number = 233546;
-		reverse(number);
-		cout << number << endl;
-		return 0;
+	int number = 233546;
+	reverse(number);
+	cout << number << endl;
+	return 0;
 }
